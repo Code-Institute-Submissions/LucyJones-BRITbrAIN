3ca8e6063eb2e2b1a2f4644ccaa744c789f8416c
Lucyjpjones <lucyjpjones@gmail.com>
2020-09-11 13:31:56 +0000

    js start quiz function added to js file with hide property

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,4 +15,24 @@ $(document).ready(function () {
   $(".btn-info").click(function () {
     $("#question-box").toggle();
   });
+
+  // Start Quiz //
+  const popupStartBox = document.getElementById("start-popup");
+  const startButton = document.getElementById("letsgo-btn");
+  const gameHeaderElement = document.getElementById("game-header");
+  const questionContainerElement = document.getElementById(
+    "question-container"
+  );
+  const answerButtonsElement = document.getElementById("answer-buttons");
+
+  startButton.addEventListener("click", startGame);
+
+  //Function to start game//
+  function startGame() {
+    popupStartBox.classList.add("hide");
+    gameHeaderElement.classList.remove("hide");
+    questionContainerElement.classList.remove("hide");
+    answerButtonsElement.classList.remove("hide");
+  }
+  
 });
