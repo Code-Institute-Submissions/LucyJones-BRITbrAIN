d4a598645520402d9d71a1ca9cca41f74849a9d1
Lucyjpjones <lucyjpjones@gmail.com>
2020-09-11 10:11:06 +0000

    js code for timer added

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  /* Code from codepen for timer*/
+  var countdownNumberEl = document.getElementById("countdown-number");
+  var countdown = 10;
+
+  countdownNumberEl.textContent = countdown;
+
+  setInterval(function () {
+    countdown = --countdown <= 0 ? 10 : countdown;
+
+    countdownNumberEl.textContent = countdown;
+  }, 1000);
+});
