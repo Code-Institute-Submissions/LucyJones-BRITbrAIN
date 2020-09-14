1ccc6202a0f6da89b47a7dd2592e622d1d7574b1
Lucyjpjones <lucyjpjones@gmail.com>
2020-09-14 17:24:50 +0000

    emailjs added to html and js file. ALl working except project request not showing on email

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,18 @@
+function sendMail(contactForm) {
+  emailjs
+    .send("lucyjpjones@gmail.com", "lucy", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.email.value,
+      project_subject: contactForm.subject.value,
+      project_request: contactForm.projectsummary.value,
+    })
+    .then(
+      function (response) {
+        console.log("SUCCESS", response);
+      },
+      function (error) {
+        console.log("FAILED", error);
+      }
+    );
+  return false; // To block from loading a new page
+}
