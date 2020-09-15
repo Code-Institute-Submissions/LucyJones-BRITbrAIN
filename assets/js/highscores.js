2b1fe4765af2e2dd6d3b8d5b98914facda793948
Lucyjpjones <lucyjpjones@gmail.com>
2020-09-15 16:36:27 +0000

    scores showing as li but li showing on browser

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -2,8 +2,11 @@ const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 highScoresList.innerText = highScores
-
   .map((score) => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+    return `
+      <li class="high-score">
+        ${score.name} - ${score.score}
+      </li>
+    `;
   })
   .join("");
