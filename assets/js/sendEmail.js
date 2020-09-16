f2dee8a3d17e017a0647e589523861d1352bb9be
Lucyjpjones <lucyjpjones@gmail.com>
2020-09-16 14:43:24 +0000

    email send working

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,21 +1,18 @@
-$(document).ready(function () {
-  function sendMail(contactForm) {
-    emailjs
-      .send("lucyjpjones@gmail.com", "lucy", {
-        from_name: contactForm.name.value,
-        from_email: contactForm.email.value,
-        project_subject: contactForm.subject.value,
-        project_request: contactForm.projectsummary.value,
-      })
-      .then(
-        function (response) {
-          console.log("SUCCESS", response);
-        },
-        function (error) {
-          console.log("FAILED", error);
-        }
-      );
-    document.getElementById("contact-form").reset();
-    return false; // To block from loading a new page
-  }
-});
+function sendMail(contactForm) {
+  emailjs
+    .send("lucyjpjones@gmail.com", "lucy", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.email.value,
+      project_subject: contactForm.subject.value,
+      project_request: contactForm.projectsummary.value,
+    })
+    .then(
+      function (response) {
+        console.log("SUCCESS", response);
+      },
+      function (error) {
+        console.log("FAILED", error);
+      }
+    );
+  return false; // To block from loading a new page
+}
