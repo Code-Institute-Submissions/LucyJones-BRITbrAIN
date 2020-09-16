ebb6ccab00eecaddf4659e868ab2fc441f71cda4
Lucyjpjones <lucyjpjones@gmail.com>
2020-09-16 09:49:40 +0000

    saving all files so nothing lost

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,33 +1,34 @@
-$(document).ready(function () {
-  const username = document.getElementById("username");
-  const submitScoreBtn = document.getElementById("submitScoreBtn");
-  const newScore = localStorage.getItem("newScore");
-  const MAX_HIGH_SCORES = 5;
+const username = document.getElementById("username");
+const submitScoreBtn = document.getElementById("submitScoreBtn");
+const endScore = document.getElementById("finalScore");
+const newScore = localStorage.getItem("newScore");
+const maxHighScores = 5;
 
-  // Get highscores or if first time get empty array //
-  const results = JSON.parse(localStorage.getItem("results")) || [];
+// Get highscores or if first time get empty array //
+const results = JSON.parse(localStorage.getItem("results")) || [];
 
-  // Submit button will not work unless username entered //
-  username.addEventListener("keyup", () => {
-    submitScoreBtn.disabled = !username.value;
-  });
+endScore.innerText = newScore;
 
-  submitHighScore = (e) => {
-    console.log("clicked the submit button!");
-    e.preventDefault();
-
-    const record = {
-      name: username.value,
-      score: newScore,
-    };
-    results.push(record);
-    // Sort scores highest to lowest //
-    results.sort((a, b) => b.score - a.score);
-    // Only keep top 5 scores //
-    results.splice(5);
-
-    localStorage.setItem("results", JSON.stringify(results));
-    // Automatically go to leaderboard after submission //
-    window.location.assign("highscores.html");
-  };
+// Submit button will not work unless username entered //
+username.addEventListener("keyup", () => {
+  submitScoreBtn.disabled = !username.value;
 });
+
+submitHighScore = (e) => {
+  console.log("clicked the submit button!");
+  e.preventDefault();
+
+  const record = {
+    name: username.value,
+    score: newScore,
+  };
+  results.push(record);
+  // Sort scores highest to lowest //
+  results.sort((a, b) => b.score - a.score);
+  // Only keep top 5 scores //
+  results.splice(5);
+
+  localStorage.setItem("results", JSON.stringify(results));
+  // Automatically go to leaderboard after submission //
+  window.location.assign("leaderboard.html");
+};
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,119 @@
 $(document).ready(function () {
+  // Toggle question //
+  $(".btn-info").click(function () {
+    $("#question-box").toggle();
+  });
+
+  // Scoring //
+
+  let score = 0;
+  const scoreCountElement = document.getElementById("score-number");
+
+  function correctScore() {
+    score += 10;
+    scoreCountElement.textContent = score;
+  }
+
+  function incorrectScore() {
+    score -= 5;
+    scoreCountElement.textContent = score;
+  }
+
+  // Progress //
+
+  let questionCount = 0;
+  const questionCountElement = document.getElementById("question-number");
+
+  function progress() {
+    questionCount++;
+    questionCountElement.textContent = questionCount;
+  }
+
+  // Start Quiz //
+  const popupStartBox = document.getElementById("start-popup");
+  const startButton = document.getElementById("letsgo-btn");
+  const gameHeaderElement = document.getElementById("game-header");
+  const questionContainerElement = document.getElementById(
+    "question-container"
+  );
+  const questionElement = document.getElementById("question");
+  const answerButtonsElement = document.getElementById("answer-buttons");
+
+  let shuffledQuestions, currentQuestionIndex;
+
+  //Event listener to start game//
+  startButton.addEventListener("click", startGame);
+
+  //Function to start game//
+  function startGame() {
+    popupStartBox.classList.add("hide");
+    gameHeaderElement.classList.remove("hide");
+    questionContainerElement.classList.remove("hide");
+    answerButtonsElement.classList.remove("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+    currentQuestionIndex = 1;
+    setInterval(countdown, 1000);
+    setNextQuestion();
+  }
+
+  // Function to show next question//
+  function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+    $("#question-box").show();
+    progress();
+  }
+  // Show question content from question array //
+  function showQuestion(question) {
+    questionElement.innerText = question.question;
+  }
+  //Timer//
+
+  let timeLeft = 10;
+  const countdownNumber = document.getElementById("countdown-number");
+
+  function countdown() {
+    timeLeft = --timeLeft <= -1 ? 10 : timeLeft;
+    countdownNumber.textContent = timeLeft;
+    if (timeLeft === 0) {
+      noTimeLeft();
+    } else {
+      countdownNumber.textContent = timeLeft;
+    }
+  }
+
+  function noTimeLeft() {
+    localStorage.setItem("newScore", score);
+    return window.location.assign("end.html");
+  }
+
+  // Select answer //
+  $(".choice").on("click", function () {
+    let userAnswer = $(this).attr("id");
+    if (userAnswer === questions[currentQuestionIndex].correctAnswer) {
+      //sweet alerts2 //
+      Swal.fire({
+        icon: "success",
+        title: "Correct Answer",
+        showConfirmButton: false,
+        timer: 1200,
+      });
+      correctScore();
+      currentQuestionIndex++;
+      setNextQuestion();
+    } else {
+      //sweet alerts2 //
+      Swal.fire({
+        icon: "error",
+        title: "Wrong Answer",
+        showConfirmButton: false,
+        timer: 1200,
+      });
+      incorrectScore();
+      currentQuestionIndex++;
+      setNextQuestion();
+    }
+  });
+
   // My questions //
   var questions = [
     {
@@ -22,126 +137,4 @@ $(document).ready(function () {
       correctAnswer: "south-west",
     },
   ];
-
-  /* Code from codepen for timer*/
-  var timeLeft = 10;
-  var countdownNumber = document.getElementById("countdown-number");
-
-  // Toggle question //
-  $(".btn-info").click(function () {
-    $("#question-box").toggle();
-  });
-
-  scoreCount = 0;
-  questionCount = 0;
-
-  // Score count //
-  function correctScore() {
-    const scoreCountElement = document.getElementById("score-number");
-    scoreCount += 10;
-    scoreCountElement.textContent = scoreCount;
-  }
-
-  function incorrectScore() {
-    const scoreCountElement = document.getElementById("score-number");
-    scoreCount -= 5;
-    scoreCountElement.textContent = scoreCount;
-  }
-
-  // Question count //
-
-  function progress() {
-    const questionCountElement = document.getElementById("question-number");
-    questionCount++;
-    questionCountElement.textContent = questionCount;
-  }
-
-  // Start Quiz //
-  const popupStartBox = document.getElementById("start-popup");
-  const startButton = document.getElementById("letsgo-btn");
-  const gameHeaderElement = document.getElementById("game-header");
-  const questionContainerElement = document.getElementById(
-    "question-container"
-  );
-  const questionElement = document.getElementById("question");
-  const answerButtonsElement = document.getElementById("answer-buttons");
-  const finalScore = document.getElementById("end-popup");
-
-  let shuffledQuestions, currentQuestionIndex;
-
-  //Event listener to start game//
-  startButton.addEventListener("click", startGame);
-
-  //Function to start game//
-  function startGame() {
-    popupStartBox.classList.add("hide");
-    gameHeaderElement.classList.remove("hide");
-    questionContainerElement.classList.remove("hide");
-    answerButtonsElement.classList.remove("hide");
-    shuffledQuestions = questions.sort(() => Math.random() - 0.5);
-    currentQuestionIndex = 1;
-    setInterval(countdown, 1000);
-    setNextQuestion();
-  }
-  //Timer//
-  function countdown() {
-    timeLeft = --timeLeft <= -1 ? 10 : timeLeft;
-    countdownNumber.textContent = timeLeft;
-    if (timeLeft === 0) {
-      noTimeLeft();
-    } else {
-      countdownNumber.textContent = timeLeft;
-    }
-  }
-
-  function noTimeLeft() {
-    const finalScoreCount = document.getElementById("finalScore");
-    finalScoreCount.textContent = scoreCount;
-    finalScore.classList.remove("hide");
-    gameHeaderElement.classList.add("hide");
-    questionContainerElement.classList.add("hide");
-    answerButtonsElement.classList.add("hide");
-    localStorage.setItem("newScore", scoreCount);
-  }
-
-  // Function to show next question//
-  function setNextQuestion() {
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
-    $("#question-box").show();
-    progress();
-  }
-  // Show question content from question array //
-  function showQuestion(question) {
-    questionElement.innerText = question.question;
-  }
-
-  // Select answer //
-  $(".choice").on("click", function () {
-    let userAnswer = $(this).attr("id");
-    if (userAnswer === questions[currentQuestionIndex].correctAnswer) {
-      //sweet alerts2 //
-      Swal.fire({
-        icon: "success",
-        title: "Correct Answer",
-        showConfirmButton: false,
-        timer: 1200,
-      });
-      currentQuestionIndex++;
-      setNextQuestion();
-      correctScore();
-    } else {
-      //sweet alerts2 //
-      Swal.fire({
-        icon: "error",
-        title: "Wrong Answer",
-        showConfirmButton: false,
-        timer: 1200,
-      });
-      currentQuestionIndex++;
-      setNextQuestion();
-      incorrectScore();
-    }
-  });
-
-  // local storage //
 });
