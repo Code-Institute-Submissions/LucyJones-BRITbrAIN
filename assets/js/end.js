f5ea7663af2eff9f8dc025e5b9cbb4786aee1f26
Lucyjpjones <lucyjpjones@gmail.com>
2020-09-16 09:58:27 +0000

    changed from top 5 to top 10 for leaderboard

--- assets/js/end.js
+++ assets/js/end.js
@@ -2,7 +2,7 @@ const username = document.getElementById("username");
 const submitScoreBtn = document.getElementById("submitScoreBtn");
 const endScore = document.getElementById("finalScore");
 const newScore = localStorage.getItem("newScore");
-const maxHighScores = 5;
+const maxHighScores = 10;
 
 // Get highscores or if first time get empty array //
 const results = JSON.parse(localStorage.getItem("results")) || [];
@@ -26,7 +26,7 @@ submitHighScore = (e) => {
   // Sort scores highest to lowest //
   results.sort((a, b) => b.score - a.score);
   // Only keep top 5 scores //
-  results.splice(5);
+  results.splice(10);
 
   localStorage.setItem("results", JSON.stringify(results));
   // Automatically go to leaderboard after submission //
